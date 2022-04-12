66aa24ec8efd470f1a77e1dec86c28ac793fed02
洪星义 <hongxingyi@yuantutech.com>
2022-04-12 14:23:50 +0800

    创建dev分支

--- forezp/sc-f-chapter8/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ forezp/sc-f-chapter8/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -17,5 +17,6 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("创建dev分支");
 	}
 }
